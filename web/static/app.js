54bc42b08aa7e52fa53bcb154753070e8a4fb955
VImaginator <vimaginator@proton.me>
2022-10-20 19:24:06 +0000

    fs/notify: don't show f_handle if exportfs_encode_inode_fh failed

--- web/static/app.js
+++ web/static/app.js
@@ -65,4 +65,66 @@ var dodata = {
     labels: tagSuccessRate.map((x)=>x._id),
     datasets: [
         {
-            dat
+            data: tagSuccessRate.map((x)=>x.count),
+            backgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56"
+            ],
+            hoverBackgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56"
+            ]
+        }],
+};
+
+if (tagctx) {
+var myDoughnutChart = new Chart(tagctx, {
+    type: 'doughnut',
+    data: dodata,
+    options: {
+        title: {
+            display: true,
+            text: "Number of prizes won"
+        }
+    }
+});
+}
+
+var popctx = document.getElementById("tagTimedPopularity");
+var popdata = {
+    labels: tagTimedPopularity.map((x)=>x._id),
+    datasets: [
+        {
+            label: "Usage over time",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: tagTimedPopularity.map((x)=>x.count),
+            spanGaps: false,
+        }
+    ]
+};
+
+if (popctx) {
+var myLineChart = new Chart(popctx, {
+    type: 'line',
+    data: popdata
+});
+}
+
